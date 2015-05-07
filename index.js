f6a2674cbb34649250bf20240bf114b2c8acb2b3
Adam Stokes <adam.stokes@ubuntu.com>
2015-05-07 00:13:41 -0400

    reorg
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- index.js
+++ index.js
@@ -1,68 +1,13 @@
-var _ = require('lodash');
-var fs = require('fs');
-var async = require('async');
-var handlebars = require('handlebars');
-var moment = require('moment');
-var metadata = require('./config');
-var posts = require('./posts');
-var mkdirp = require('mkdirp');
+var Bummyjab = require('./lib');
+var config = require('./config');
 
-var partials = ['header', 'footer', 'sidebar'];
-_.each(partials, function (partial) {
-  handlebars.registerPartial(partial, fs.readFileSync(__dirname + '/templates/partials/' + partial + '.hbs')
-    .toString());
-});
+var bj = Bummyjab();
 
-handlebars.registerHelper('date', function (date) {
-  return moment(date)
-    .format('Do MMMM YYYY');
-});
+// define partials
+bj.partials(['footer.hbs', 'header.hbs', 'sidebar.hbs']);
 
-handlebars.registerHelper('xmldate', function (date) {
-  return moment(date)
-    .format('ddd, DD MMM YYYY HH:mm:ss ZZ');
-});
+// setup metadata
+bj.metadata(config);
 
-handlebars.registerHelper('sitemapdate', function (date) {
-  return moment(date)
-    .format('YYYY-MM-DD');
-});
-
-handlebars.registerHelper('link', function (path) {
-  return metadata.baseUrl + '/' + path;
-});
-
-var items = posts.loadPosts();
-
-mkdirp('build', function (err) {
-  if (err) {
-    throw (err);
-  }
-});
-
-async.parallel([
-  function (callback) {
-    posts.genIndex(items, function (err) {
-      callback(err);
-    });
-  },
-  function (callback) {
-    posts.genFeed(items, function (err) {
-      callback(err);
-    });
-  },
-  function (callback) {
-    posts.genUbuntuFeed(items, function (err) {
-      callback(err);
-    });
-  },
-  function (callback) {
-    posts.genPosts(items, function (err) {
-      callback(err);
-    });
-  }
-], function (err, callback) {
-  if (err) {
-    console.error(err);
-  }
-});
+console.log(bj.metadata());
+console.log(bj.partials());
