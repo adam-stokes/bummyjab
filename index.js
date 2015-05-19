bc12bd06f08a48c2d9821253f2e50b7052793ef5
Adam Stokes <adam.stokes@ubuntu.com>
2015-05-19 01:58:12 -0400

    more code reorg

--- index.js
+++ index.js
@@ -1,44 +1,6 @@
-var _ = require('lodash');
-var path = require('path');
-var fs = require('fs');
 var async = require('async');
-var handlebars = require('handlebars');
-var moment = require('moment');
-var metadata = require('./config');
 var posts = require('./lib/posts');
-var mkdirp = require('mkdirp');
 var appRoot = require('app-root-path');
-
-var partials = ['header', 'footer', 'sidebar'];
-_.each(partials, function (partial) {
-  handlebars.registerPartial(partial, fs.readFileSync(appRoot + '/templates/partials/' + partial + '.hbs')
-    .toString());
-});
-
-handlebars.registerHelper('date', function (date) {
-  return moment(date)
-    .format('Do MMMM YYYY');
-});
-
-handlebars.registerHelper('xmldate', function (date) {
-  return moment(date)
-    .format('ddd, DD MMM YYYY HH:mm:ss ZZ');
-});
-
-handlebars.registerHelper('sitemapdate', function (date) {
-  return moment(date)
-    .format('YYYY-MM-DD');
-});
-
-handlebars.registerHelper('link', function (path) {
-  return metadata.baseUrl + '/' + path;
-});
-
-mkdirp('build', function (err) {
-  if (err) {
-    throw (err);
-  }
-});
 
 async.map(process.argv.slice(2), function (item, callback) {
   posts.loadPosts(item, function (err, res) {
